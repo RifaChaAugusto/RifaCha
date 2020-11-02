d384aa538c759943d21a6cb1a86f8d42817d9e41
DavidsonGontijo <d.gontj@gmail.com>
2020-11-02 19:09:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ function Apagafoto(){
 $(document).ready(function(){
 	document.getElementById("idImg").style.visibility="Hidden";
 	document.getElementById("idMini").style.visibility="Hidden";
-    //$.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
-    $.getJSON( "params.json", function() {		
+    //$.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {	
+    //$.getJSON( "params.json", function() {
+    $.getJSON( "http://github.com/RifaChaAugusto/RifaCha/blob/main/params.json", function() {
     }).done(function(data) { 
         var content = '<option>Pesquisar Currículo de</option>'; 
         for(var i=0; i <data.length; i++){
@@ -30,7 +31,8 @@ $(document).ready(function(){
 			document.getElementById("idMini").style.visibility="Visible";
             str = $( this ).text();		
 	    	//$.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
-		$.getJSON( "params.json", function() { 
+		//$.getJSON( "params.json", function() {
+		$.getJSON( "http://github.com/RifaChaAugusto/RifaCha/blob/main/params.json", function() { 
         	}).done(function(data) {
         	    for(var i=0; i <data.length; i++){
                      var nomeAluno = data[i]['nome'] 					 
