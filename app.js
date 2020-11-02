7f6357a102443b69639101d651dffe3b9dd7446b
DavidsonGontijo <d.gontj@gmail.com>
2020-11-02 16:28:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ function Apagafoto(){
 $(document).ready(function(){
 	document.getElementById("idImg").style.visibility="Hidden";
 	document.getElementById("idMini").style.visibility="Hidden";
-    $.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
+    //$.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
+    $.getJSON( "params.json", function() {		
     }).done(function(data) { 
         var content = '<option>Pesquisar Currículo de</option>'; 
         for(var i=0; i <data.length; i++){
